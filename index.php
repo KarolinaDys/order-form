4510f1f2bed7e4743524df3d3a84a2a3fda539eb
Karolina Dys <karolinadyss@gmail.com>
2020-09-18 09:39:46 +0200

    add change step 4

--- index.php
+++ index.php
@@ -54,16 +54,18 @@ function validateInput()
             
           }
 */
-
-
-
 // define variables and set to empty values
 $emailErr = $streetErr = $streetnumberErr = $cityErr = $zipcodeErr = $productsErr = "";
 $email = $street = $streetnumber = $city = $zipcode = $products = "";
-$isFormValid = true;
+$isFormValid;
+$submit = "";
+$deliveryTime = "";
+$totalMessage = "";
+$total = 0;
 
 // Validate and check requirements form
 if (!empty($_POST)) {
+
   //email
   if (empty($_POST["email"])) {
     $emailErr = "Email is required";
@@ -91,7 +93,6 @@ if (!empty($_POST)) {
     $isFormValid = false;
   } else {
     $streetnumber = test_input($_POST["streetnumber"]);
-    // check if the streetnumber only consists out of numbers
     if (!preg_match("/^[0-9]*$/", $streetnumber)) {
       $streetnumberErr = "Only numbers are allowed";
       $isFormValid = false;
@@ -112,7 +113,6 @@ if (!empty($_POST)) {
     $isFormValid = false;
   } else {
     $zipcode = test_input($_POST["zipcode"]);
-    // check if the zipcode only consists out of numbers
     if (!preg_match("/^[0-9]*$/", $zipcode)) {
       $zipcodeErr = "Only numbers are allowed";
     }
@@ -123,7 +123,18 @@ if (!empty($_POST)) {
     $productsErr = "Pick at least one product";
     $isFormValid = false;
   } else {
-    $products = test_input($_POST["products"]);
+    $products = $_POST["products"];
+  }
+
+  // The form is valid
+  if ($isFormValid = true) {
+    createSessionValues();
+
+    $submit = 'Thank you! Your order is submitted. </br>';
+
+    $deliveryTime = estimateDeliveryTime();
+    $total = totalAmountPerOrder();
+    $totalMessage = 'The total amount of this order is €' . totalAmountPerOrder() . "</br>";
   }
 };
 
@@ -145,30 +156,15 @@ if (isset($_SESSION["zipcode"])) {
   $zipcode = $_SESSION["zipcode"];
 };
 
-// The form is valid
-$submit = "";
-$deliveryTime = "";
-$totalMessage = "";
-$total = 0;
-
-if ($isFormValid) {
-  createSessionValues();
-
-  $submit = 'Thank you! Your order is submitted. </br>';
-
-  $deliveryTime = estimateDeliveryTime();
-  $total = totalAmountPerOrder();
-  $totalMessage = 'The total amount of this order is €' . totalAmountPerOrder() . "</br>";
-};
 
 // Create the session values
 function createSessionValues()
 {
   $_SESSION["email"] = $_POST["email"];
-  $_SESSION["street"] = $_POST["street"];
-  $_SESSION["streetnumber"] = $_POST["streetnumber"];
-  $_SESSION["city"] = $_POST["city"];
-  $_SESSION["zipcode"] = $_POST["zipcode"];
+  $_SESSION["street"] = test_input($_POST["street"]);
+  $_SESSION["streetnumber"] = test_input($_POST["streetnumber"]);
+  $_SESSION["city"] = test_input($_POST["city"]);
+  $_SESSION["zipcode"] = test_input($_POST["zipcode"]);
 };
 
 //your products with their price.
@@ -189,17 +185,6 @@ if (isset($_GET["drinks"]) && $_GET['drinks'] == 1) {
   ];
 };
 
-// Calculate the total amount per order
-function totalAmountPerOrder()
-{
-  $totalValue = 0;
-  foreach ($_POST["products"] as $value) {
-    // echo "$value <br>";
-    $totalValue += $value;
-  }
-
-  return $totalValue;
-}
 
 // Delivery time
 function estimateDeliveryTime()
@@ -214,35 +199,44 @@ function estimateDeliveryTime()
     return "Your order will be delivered at " . $hourNormalDel . "</br>";
   };
 }
+// Calculate the total amount per order
+function totalAmountPerOrder()
+{
+  $totalValue = 0;
+  foreach ($_POST["products"] as $value) {
+    // echo "$value <br>";
+    $totalValue += $value;
+  }
 
+  return $totalValue;
+};
 //Total value with cookies
 $totalAmount = 0;
-
 if (isset($_COOKIE['amountOverall'])) {
-  $totalAmount = $_COOKIE['amountOverall'] + $total;
+  $totalAmount = $_COOKIE['amountOverall'] + $value;
 };
 
 $cookie = "$totalAmount";
-setcookie("amountOverall", $cookie, time() + (86400 * 30), "/"); // 86400 = 1 day
+setcookie("amountOverall", $cookie, time() + (86400 * 30), "/");
 
-//Function to validate the input
-/* Validate Form Data With PHP
-The first thing we will do is to pass all variables through PHP's htmlspecialchars() function.
-When we use the htmlspecialchars() function; then if a user tries to submit the following in a text field:
-<script>location.href('http://www.hacked.com')</script>
-- this would not be executed, because it would be saved as HTML escaped code, like this:
-&lt;script&gt;location.href('http://www.hacked.com')&lt;/script&gt;
-The code is now safe to be displayed on a page or inside an e-mail.
-We will also do two more things when the user submits the form:
-Strip unnecessary characters (extra space, tab, newline) from the user input data (with the PHP trim() function)
-Remove backslashes (\) from the user input data (with the PHP stripslashes() function)
-The next step is to create a function that will do all the checking for us (which is much more convenient than writing the same code over and over again). */
 function test_input($data)
 {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
+}
+// mail
+$to = $email;
+$subject = 'Your order';
+$message = ""; 
+$from = 'peterparker@email.com';
+ 
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
 }
 
 // Debug function what is happening
